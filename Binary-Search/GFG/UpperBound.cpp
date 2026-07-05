662fa1ddafdc0e30e2a8fa98ec1c0c0ab82faf13
hardik0903 <hardikpandey0903@gmail.com>
2026-07-05 23:36:35 +0530

    Add Binary-Search/GFG: UpperBound

---
+++ Binary-Search/GFG/UpperBound.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int UpperBound(vector<int>& arr,int target){
+    int n=arr.size();
+    int low=0;
+    int high=n-1;
+    int ans=n;
+
+    while(low<=high){
+        int mid=(low+high)/2;
+
+        if(arr[mid]>target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> arr1={3,5,8,15};
+    int answer1=UpperBound(arr1,8);
+
+    cout<<"Calculated Answer is: "<<answer1<<" Whereas Real Answer is: 3"<<endl;;
+
+    vector<int> arr2={1,3,5,7,9,9,9,9,10};
+    int answer2=UpperBound(arr2,9);
+
+    cout<<"Calculated Answer is: "<<answer2<<" Whereas Real Answer is: 8"<<endl;
+
+    vector<int> arr3={1,3,5,7,9,10,12};
+    int answer3=UpperBound(arr3,14);
+
+    cout<<"Calculated Answer is: "<<answer3<<" Whereas Real Answer is: 7"<<endl;
+
+    vector<int> arr4={2,3,4,5,6,7};
+    int answer4=UpperBound(arr4,1);
+
+    cout<<"Calculated Answer is: "<<answer4<<" Whereas Real Answer is: 0"<<endl;
+return 0;
+}
+
+
