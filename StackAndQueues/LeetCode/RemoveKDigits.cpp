0ded2affce63623ea84e4a3d2ad984fc49024ad5
hardik0903 <hardikpandey0903@gmail.com>
2026-07-05 23:36:44 +0530

    Add StackAndQueues/LeetCode: RemoveKDigits

---
+++ StackAndQueues/LeetCode/RemoveKDigits.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        int n = num.size();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && k > 0 && (st.top() - '0') > (num[i] - '0')) {
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+
+        while (!st.empty() && k != 0) {
+            st.pop();
+            k--;
+        }
+
+        if (st.empty()) return "0";
+
+        string res = "";
+
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+
+        while (res.size() != 0 && res.back() == '0') {
+            res.pop_back();
+        }
+
+        reverse(res.begin(), res.end());
+
+        if (res.size() == 0) return "0";
+
+        return res;
+    }
+};
+
+int main() {
+    string num = "1432219";
+    int k = 3;
+    Solution sol;
+    cout << sol.removeKdigits(num, k) << endl;
+    return 0;
+}
