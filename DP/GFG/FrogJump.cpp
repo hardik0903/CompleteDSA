97f252ca381a2ddd8831856e587f7cd9d6fa09fd
hardik0903 <hardikpandey0903@gmail.com>
2026-07-17 10:40:01 +0530

    Update DP/GFG: FrogJump

--- DP/GFG/FrogJump.cpp
+++ DP/GFG/FrogJump.cpp
@@ -20,24 +20,44 @@ public:
     //     return f(n-1, heights);
     // }
 
-    int f(int index, vector<int>& heights, vector<int>& dp){
-        if(index==0) return 0;
-        if(dp[index]!=-1) return dp[index];
+    //!MEMOIZATION APPROACH
+    // int f(int index, vector<int>& heights, vector<int>& dp){
+    //     if(index==0) return 0;
+    //     if(dp[index]!=-1) return dp[index];
 
-        int left = f(index-1, heights, dp) + abs(heights[index] - heights[index-1]);
-        int right=INT_MAX;
-        if(index>1){
-            right =f(index-2, heights, dp) + abs(heights[index] - heights[index-2]);
-        }
+    //     int left = f(index-1, heights, dp) + abs(heights[index] - heights[index-1]);
+    //     int right=INT_MAX;
+    //     if(index>1){
+    //         right =f(index-2, heights, dp) + abs(heights[index] - heights[index-2]);
+    //     }
 
-        return dp[index] = min(left, right);
-    }
+    //     return dp[index] = min(left, right);
+    // }
 
+    // int frogJump(vector<int>& heights){
+    //     int n=heights.size();
+    //     vector<int> dp(n, -1);
+
+    //     return f(n-1, heights, dp);
+    // }
+
+    //!TABULATION APPROACH
     int frogJump(vector<int>& heights){
         int n=heights.size();
-        vector<int> dp(n, -1);
 
-        return f(n-1, heights, dp);
+        vector<int> dp(n, 0);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            int fs = dp[i-1] + abs(heights[i] - heights[i-1]);
+            int rs=INT_MAX;
+
+            if(i>1){
+                rs = dp[i-2] + abs(heights[i-2] - heights[i]); 
+            }
+
+            dp[i] = min(fs, rs);
+        }
+        return dp[n-1];
     }
 };
 
