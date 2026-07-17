3e8b053a8a81646f14102554af127235f36e1d2e
hardik0903 <hardikpandey0903@gmail.com>
2026-07-17 10:23:23 +0530

    Update DP/GFG: FrogJump

--- DP/GFG/FrogJump.cpp
+++ DP/GFG/FrogJump.cpp
@@ -24,10 +24,10 @@ public:
         if(index==0) return 0;
         if(dp[index]!=-1) return dp[index];
 
-        int left = f(index-1, heights, dp);
+        int left = f(index-1, heights, dp) + abs(heights[index] - heights[index-1]);
         int right=INT_MAX;
         if(index>1){
-            right =f(index-2, heights, dp);
+            right =f(index-2, heights, dp) + abs(heights[index] - heights[index-2]);
         }
 
         return dp[index] = min(left, right);
