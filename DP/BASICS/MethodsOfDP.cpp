3de1ef0a74b9a4f3527f2392d8f08c8e1380a295
hardik0903 <hardikpandey0903@gmail.com>
2026-07-21 09:01:02 +0530

    Update DP/BASICS: MethodsOfDP

--- DP/BASICS/MethodsOfDP.cpp
+++ DP/BASICS/MethodsOfDP.cpp
@@ -16,7 +16,7 @@ public:
     int fibonacci(int n, vector<int>& dp){
         if(n<=1) return n;
 
-        if(dp[n]!=1) return dp[n];
+        if(dp[n]!=-1) return dp[n];
 
         return dp[n]=fibonacci(n-1, dp)+fibonacci(n-2, dp);
     }
