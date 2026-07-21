b5306715c3100e6191d4ca3b8adf5fac77e82163
hardik0903 <hardikpandey0903@gmail.com>
2026-07-21 09:53:18 +0530

    Update DP/BASICS: MethodsOfDP

--- DP/BASICS/MethodsOfDP.cpp
+++ DP/BASICS/MethodsOfDP.cpp
@@ -25,7 +25,7 @@ public:
         dp[0]=0;
         dp[1]=1;
 
-        for(int i=2;i<number;i++){
+        for(int i=2;i<=number;i++){
             dp[i] = dp[i-1]+dp[i-2];
         }
         return dp[number];
