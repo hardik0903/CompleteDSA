efd682c8197b07c9553fcde04af330305971c866
hardik0903 <hardikpandey0903@gmail.com>
2026-07-21 08:56:24 +0530

    Update DP/BASICS: MethodsOfDP

--- DP/BASICS/MethodsOfDP.cpp
+++ DP/BASICS/MethodsOfDP.cpp
@@ -14,12 +14,11 @@ public:
 
     //!FIRST DP APPROACH: MEMOIZATION
     int fibonacci(int n, vector<int>& dp){
-        dp[0]=0;
-        dp[1]=1;
+        if(n<=1) return n;
 
         if(dp[n]+=-1) return dp[n];
 
-        return fibonacci(n-1)+fibonacci(n-2);
+        return dp[n]=fibonacci(n-1)+fibonacci(n-2);
     }
 };
 
