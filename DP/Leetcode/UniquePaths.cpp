45bdf4c5b725da2d0e08d12703cd768ffa9fbc1c
hardik0903 <hardikpandey0903@gmail.com>
2026-07-25 17:02:47 +0530

    Update DP/Leetcode: UniquePaths

--- DP/Leetcode/UniquePaths.cpp
+++ DP/Leetcode/UniquePaths.cpp
@@ -3,19 +3,39 @@ using namespace std;
 
 class Solution{
 public:
-    int f(int ind1, int ind2){
+
+    //!RECURSION APPROACH
+    // int f(int ind1, int ind2){
+
+    //     if(ind1==0 && ind2==0) return 1;
+    //     if(ind1<0 || ind2<0) return 0;
+
+    //     int up = f(ind1-1, ind2);
+    //     int left = f(ind1, ind2-1);
+
+    //     return up+left;
+    // }
+
+    // int uniquePaths(int m, int n){
+    //     return f(m-1, n-1);
+    // }
+
+    //!MEMOIZATION
+    int f(int ind1, int ind2, vector<vector<int>>& dp){
 
         if(ind1==0 && ind2==0) return 1;
         if(ind1<0 || ind2<0) return 0;
 
-        int up = f(ind1-1, ind2);
-        int left = f(ind1, ind2-1);
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        int up = f(ind1-1, ind2, dp);
+        int left = f(ind1, ind2-1, dp);
 
-        return up+left;
+        return dp[ind1][ind2]=up+left;
     }
 
     int uniquePaths(int m, int n){
-        return f(m-1, n-1);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m-1, n-1, dp);
     }
 };
 
