aec86e18dc284bb21e86c374fa03719dbb4b5cea
hardik0903 <hardikpandey0903@gmail.com>
2026-07-25 16:59:52 +0530

    Add DP/Leetcode: UniquePaths

---
+++ DP/Leetcode/UniquePaths.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int f(int ind1, int ind2){
+
+        if(ind1==0 && ind2==0) return 1;
+        if(ind1<0 || ind2<0) return 0;
+
+        int up = f(ind1-1, ind2);
+        int left = f(ind1, ind2);
+
+        return up+left;
+    }
+
+    int uniquePaths(int m, int n){
+        return f(m-1, n-1);
+    }
+};
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+
+    Solution sol;
+    int ans = sol.uniquePaths(m, n);
+    cout<<ans;
+}
