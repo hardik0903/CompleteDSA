4f304dd686949e10a2b911978a5c6ee287d24a6f
hardik0903 <hardikpandey0903@gmail.com>
2026-07-25 17:05:17 +0530

    Update DP/Leetcode: UniquePaths

--- DP/Leetcode/UniquePaths.cpp
+++ DP/Leetcode/UniquePaths.cpp
@@ -21,21 +21,42 @@ public:
     // }
 
     //!MEMOIZATION
-    int f(int ind1, int ind2, vector<vector<int>>& dp){
+    // int f(int ind1, int ind2, vector<vector<int>>& dp){
 
-        if(ind1==0 && ind2==0) return 1;
-        if(ind1<0 || ind2<0) return 0;
+    //     if(ind1==0 && ind2==0) return 1;
+    //     if(ind1<0 || ind2<0) return 0;
 
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        int up = f(ind1-1, ind2, dp);
-        int left = f(ind1, ind2-1, dp);
+    //     if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+    //     int up = f(ind1-1, ind2, dp);
+    //     int left = f(ind1, ind2-1, dp);
 
-        return dp[ind1][ind2]=up+left;
-    }
+    //     return dp[ind1][ind2]=up+left;
+    // }
 
+    // int uniquePaths(int m, int n){
+    //     vector<vector<int>> dp(m, vector<int>(n, -1));
+    //     return f(m-1, n-1, dp);
+    // }
+
+    //!TABULATION
     int uniquePaths(int m, int n){
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0]=1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else{
+                    int up=0, left=0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
 
