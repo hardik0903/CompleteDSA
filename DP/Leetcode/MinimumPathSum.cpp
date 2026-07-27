8847d0a8efd701e58cc4ba364dc0ca23682eefa8
hardik0903 <hardikpandey0903@gmail.com>
2026-07-27 09:17:19 +0530

    Update DP/Leetcode: MinimumPathSum

--- DP/Leetcode/MinimumPathSum.cpp
+++ DP/Leetcode/MinimumPathSum.cpp
@@ -4,28 +4,49 @@ using namespace std;
 class Solution{
 public:
     
+    //!RECURSION APPROACH
+    int f(int i, int j, vector<vector<int>>& grid){
+
+        if(i==0 && j==0){
+            return grid[0][0];
+        }
+
+        if(i<0 || j<0) return INT_MAX;
+
+        int up = grid[i][j] + f(i-1, j, grid);
+        int left = grid[i][j] + f(i, j-1, grid);
+
+        return min(up, left);
+    }
     int minimumPathSum(vector<vector<int>>& grid){
         int n=grid.size();
         int m=grid[0].size();
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-
-                if(i==0 && j==0) dp[i][j] = grid[i][j];
-
-                else{
-                    int up = INT_MAX, left = INT_MAX;
-                    if(i>0) up = grid[i][j] + dp[i-1][j];
-                    if(j>0) left = grid[i][j] + dp[i][j-1];
-
-                    dp[i][j] = min(up, left);
-                }
-            }
-        }
-        return dp[n-1][m-1];
+        return f(n-1, m-1, grid);
     }
+    //!TABULATION APPROACH
+    // int minimumPathSum(vector<vector<int>>& grid){
+    //     int n=grid.size();
+    //     int m=grid[0].size();
+
+    //     vector<vector<int>> dp(n, vector<int>(m, 0));
+
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+
+    //             if(i==0 && j==0) dp[i][j] = grid[i][j];
+
+    //             else{
+    //                 int up = INT_MAX, left = INT_MAX;
+    //                 if(i>0) up = grid[i][j] + dp[i-1][j];
+    //                 if(j>0) left = grid[i][j] + dp[i][j-1];
+
+    //                 dp[i][j] = min(up, left);
+    //             }
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
 };
 
 int main(){
