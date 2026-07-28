f2e97ee3750afe9317640defaf38714153c4ef11
hardik0903 <hardikpandey0903@gmail.com>
2026-07-28 15:32:40 +0530

    Update DP/Leetcode: Triangle

--- DP/Leetcode/Triangle.cpp
+++ DP/Leetcode/Triangle.cpp
@@ -3,25 +3,44 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
+
+
+    //!RECURSION APPROACH
+    int f(int ind1, int ind2, vector<vector<int>>& triangle){
         int n=triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        if(ind1==n-1) return triangle[n-1][ind2];
 
-        for(int j=0;j<n;j++){
-            dp[n-1][j] = triangle[n-1][j];
-        }
+        int down = triangle[ind1][ind2] + f(ind1+1, ind2, triangle);
+        int diagonal = triangle[ind1][ind2] + f(ind1+1, ind2+1, triangle);
 
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j] + dp[i+1][j+1];
+        return min(down, diagonal);
 
-                dp[i][j] = min(down, diagonal);
-            }
-        }
-
-        return dp[0][0];
     }
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        int n = triangle.size();
+        return f(0, 0, triangle);
+    }
+    //!TABULATION APPROACH
+    // int minimumTotal(vector<vector<int>>& triangle) {
+    //     int n=triangle.size();
+    //     vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    //     for(int j=0;j<n;j++){
+    //         dp[n-1][j] = triangle[n-1][j];
+    //     }
+
+    //     for(int i=n-2;i>=0;i--){
+    //         for(int j=i;j>=0;j--){
+    //             int down = triangle[i][j] + dp[i+1][j];
+    //             int diagonal = triangle[i][j] + dp[i+1][j+1];
+
+    //             dp[i][j] = min(down, diagonal);
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
 };
 
 int main(){
