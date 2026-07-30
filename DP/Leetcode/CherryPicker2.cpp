04be0ff82f6bfe5f3c8a81b4009566d7e9956f64
hardik0903 <hardikpandey0903@gmail.com>
2026-07-30 12:05:09 +0530

    Update DP/Leetcode: CherryPicker2

--- DP/Leetcode/CherryPicker2.cpp
+++ DP/Leetcode/CherryPicker2.cpp
@@ -3,33 +3,46 @@ using namespace std;
 
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(j<0 || j>=matrix[0].size()) return 1e9;
-        if(i==0) return matrix[i][j];
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+    int f(int i,int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp){
+        int n=grid.size();
+        int m=grid[0].size();
+        if(j1<0 || j1>=m || j2<0 || j2>=m){
+            return -1e9;
         }
 
-        int up = matrix[i][j] + f(i-1, j, matrix, dp);
-        int ld = matrix[i][j] + f(i-1, j-1, matrix, dp);
-        int rd = matrix[i][j] + f(i-1, j+1, matrix, dp);
+        if(i==n-1){
+            if(j1==j2){
+                return grid[i][j1];
+            }
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
 
-        return dp[i][j] = min(up, min(ld, rd));
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int maxi=INT_MIN;
+        for(int d1=-1;d1<2;d1++){
+            for(int d2=-1;d2<2;d2++){
 
+                if(j1==j2){
+                    maxi = max(maxi, grid[i][j1] + f(i+1, j1+d1, j2+d2, grid, dp));
+                }
+                else{
+                    maxi = max(maxi, grid[i][j1] + grid[i][j2] + f(i+1, j1+d1, j2+d2, grid, dp));
+                }
+            }
+        }
+
+        return dp[i][j1][j2] = maxi;
     }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int minAns=INT_MAX;
-        int n = matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        for(int j=0;j<matrix[0].size();j++){
-            int ans = f(matrix.size()-1, j, matrix, dp);
-            minAns= min(minAns, ans);
-        }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m=grid[0].size();
 
-        return minAns;
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+
+        return f(0, 0, m-1, grid, dp);
     }
 };
 
@@ -45,7 +58,7 @@ int main(){
     }
 
     Solution sol;
-    int ans = sol.minFallingPathSum(arr);
+    int ans = sol.cherryPickup(arr);
     cout<<ans<<endl;
 }
 
