443e55728d0baad830e37a4fb6bfe6eae7347990
hardik0903 <hardikpandey0903@gmail.com>
2026-08-03 11:44:03 +0530

    Update DP/GFG: SubsetSumProblem

--- DP/GFG/SubsetSumProblem.cpp
+++ DP/GFG/SubsetSumProblem.cpp
@@ -26,27 +26,58 @@ public:
     // }
 
     //!MEMOIZATION APPROACH
-    bool f(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
+    // bool f(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
 
-        if(target==0) return true;
-        if(ind==0) return nums[0] == target;
+    //     if(target==0) return true;
+    //     if(ind==0) return nums[0] == target;
 
-        if(dp[ind][target]!=-1) return dp[ind][target];
+    //     if(dp[ind][target]!=-1) return dp[ind][target];
 
-        bool notTake = f(ind-1, target, nums, dp);
-        bool take = false;
+    //     bool notTake = f(ind-1, target, nums, dp);
+    //     bool take = false;
 
-        if(nums[ind]<=target){
-            take = f(ind-1, target - nums[ind], nums, dp);
+    //     if(nums[ind]<=target){
+    //         take = f(ind-1, target - nums[ind], nums, dp);
+    //     }
+
+    //     return dp[ind][target] = take || notTake;
+    // }
+
+    // bool subsetSumTarget(vector<int>& nums, int target){
+    //     int n=nums.size();
+    //     vector<vector<int>> dp(n, vector<int> (target+1, -1));
+    //     return f(n-1, target, nums, dp);
+    // }
+
+    //! TABULATION APPROACH
+
+    bool subsetSumTarget(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<vector<bool>> dp(n, vector<bool>(target+1, 0));
+
+        for(int i=0;i<n;i++){
+            dp[n][0] = 1;
         }
 
-        return dp[ind][target] = take || notTake;
-    }
+        if(target>=nums[0]){
+            dp[0][nums[0]] = 1;
+        }
 
-    bool subsetSumTarget(vector<int>& nums, int target, vector<vector<int>>& dp){
-        int n=nums.size();
-        
-        return f(n-1, target, nums, dp);
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+
+                bool notTake = dp[i-1][target];
+                bool take=false;
+
+                if(target>=nums[i]){
+                    take = dp[i-1][target-nums[i]];
+                }
+
+                dp[i][j] = take || notTake;
+            }
+        }
+
+        dp[n-1][target];
     }
 };
 
@@ -61,19 +92,12 @@ int main(){
     int n=nums.size();
     int k;
     cin>>k;
-    vector<vector<int>> dp(n, vector<int> (k+1, -1));
+    
     Solution sol;
-    if(sol.subsetSumTarget(nums, k, dp)){
+    if(sol.subsetSumTarget(nums, k)){
         cout<<"Element exists"<<endl;
     }
     else{
         cout<<"Element doesn't exists"<<endl;
-    }
-
-    for(int i=0;i<dp.size();i++){
-        for(int j=0;j<dp[0].size();j++){
-            cout<<dp[i][j]<<" ";
-        }
-        cout<<endl;
     }
 }
