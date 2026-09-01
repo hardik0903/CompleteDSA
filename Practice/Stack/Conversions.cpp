c5e42973eb8e25976507c1c7840987957deb2b4d
hardik0903 <hardikpandey0903@gmail.com>
2026-09-01 12:25:48 +0530

    Add Practice/Stack: Conversions

---
+++ Practice/Stack/Conversions.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    string conversion(string s){
+        stack<char> st;
+        string ans;
+        for(char& ch: s){
+            if(ch=='('){
+                st.push(ch);
+            }
+
+            if(ch==')'){
+                while(st.top()=='('){
+                    ans += st.top();
+                    st.pop();
+                }
+                st.pop();
+            }
+
+            if(ch=='^'){
+                st.push(ch);
+            }
+
+            if(ch=='*' || ch=='/'){
+                if(st.top()=='+' || st.top()=='-'){
+                    st.push(ch);
+                }
+                while(st.top()!='+' || st.top()!='-' || !st.empty()){
+                    ans+=st.top();
+                    st.pop();
+                }
+                if(st.top()=='+' || st.top()=='-'){
+                    st.push(ch);
+                }
+            }
+
+            if(ch=='+' || ch=='-'){
+                if(st.empty()){
+                    st.push(ch);
+                }
+                while(!st.empty()){
+                    ans+=st.top();
+                    st.pop();
+                }
+                if(st.empty()){
+                    st.push(ch);
+                }
+            }
+        }
+
+        while(st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
+
+int main(){
+
+    string s = "a+b*c+d";
+    Solution sol;
+    string ans = sol.conversion(s);
+    cout<<ans<<endl;
+}
